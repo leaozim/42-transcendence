87ca26aab10d1765a382b33581aa64fe9bba5832
larissa <larissa_silva@outlook.com>
2023-12-06 14:32:13 -0300

    added updates

--- src/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
-]
