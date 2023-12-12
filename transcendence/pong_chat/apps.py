27503f9b63a070ad81cd4fdf1a1a3cccce6ad7d2
larissa <larissa_silva@outlook.com>
2023-12-12 09:45:41 -0300

    renamed folders

--- transcendence/pong_chat/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PongChatConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pong_chat'
---
+++ transcendence/srcs_core/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('admin/', admin.site.urls),
+    path('srcs_users/', include(('srcs_users.urls', 'srcs_users'))),
+    path('srcs_users/', include('django.contrib.auth.urls')),
+]
