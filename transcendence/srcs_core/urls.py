27bee6b6357586a6b4090ccb6e0f58b1cac8b9b7
larissa <larissa_silva@outlook.com>
2023-12-12 14:58:49 -0300

    added intra login

--- transcendence/srcs_core/urls.py
+++ transcendence/srcs_core/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from srcs_users import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
-    path('srcs_users/', include(('srcs_users.urls', 'srcs_users'))),
-    path('srcs_users/', include('django.contrib.auth.urls')),
+    path('oauth2/', views.home, name='oauth2'),
+    path('oauth2/login/', views.intra_login, name='intra_login'),
+    path('oauth2/login/redirect', views.intra_login_redirect, name='intra_login_redirect'),
+
+    # path('srcs_users/', include(('srcs_users.urls', 'srcs_users'))),
+    # path('srcs_users/', include('django.contrib.auth.urls')),
 ]
--- transcendence/srcs_users/views.py
+++ transcendence/srcs_users/views.py
@@ -1,10 +1,50 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-
+from django.shortcuts import redirect
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from .forms import UserCreationForm
+import requests
 
+auth_url_intra =  "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-fec16bce7005bda3749ca50ff01b5f0c8fcf8964b552af66aa05c0dc76a7c485&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Foauth2%2Flogin%2Fredirect&response_type=code"
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+def home(request: HttpRequest) -> JsonResponse:
+    return JsonResponse({ "msg":  "helo"})
+
+def intra_login(request: HttpRequest): 
+    return redirect(auth_url_intra)
+
+
+def intra_login_redirect(request: HttpRequest):
+    code = request.GET.get('code')
+    print( "code = ",code)
+    user = exchange_code(code)
+    return JsonResponse({ "msg":  user})
+
+
+def exchange_code(code: str):
+    data = {
+    "client_id": "u-s4t2ud-fec16bce7005bda3749ca50ff01b5f0c8fcf8964b552af66aa05c0dc76a7c485",
+    "client_secret": "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",
+    "grant_type": "authorization_code",
+    "code": code,
+    "redirect_uri": "http://localhost:8000/oauth2/login/redirect",
+    "scope": "identify"
+    }
+    headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    response = requests.post("https://api.intra.42.fr/oauth/token", data=data, headers=headers)
+    print(response)
+    credentials = response.json()
+    access_token = credentials['access_token']
+    response = requests.get('https://api.intra.42.fr/v2/me', headers={
+    'Authorization': 'Bearer %s' % access_token
+    })
+    user = response.json()
+    print(user['email'])  
+    return user
