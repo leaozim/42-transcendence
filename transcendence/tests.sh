120219be16252742e48da21ca3c59f7982d14296
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2023-12-27 00:47:01 -0300

    removing trash

--- transcendence/tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#cavalinho
-coverage3 run --source='.' manage.py test && coverage3 report && coverage3 xml
-# docker run --rm --network=42-transcendence_sonarnet -e SONAR_HOST_URL="http://sonarqube:9000" -e SONAR_SCANNER_OPTS="-Dsonar.projectKey=transcendence -Dsonar.python.coverage.reportPaths=./coverage.xml" -e SONAR_TOKEN="sqp_270bf39358c1c28bc30df3f4758562643a67a6d1" -v ".:/usr/src" sonarsource/sonar-scanner-cli
