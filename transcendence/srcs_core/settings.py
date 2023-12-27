6b47f07fdada5a410253554df045e581240ace3b
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2023-12-26 23:26:47 -0300

    last finally

--- transcendence/srcs_core/settings.py
+++ transcendence/srcs_core/settings.py
@@ -93,11 +93,11 @@ WSGI_APPLICATION = 'srcs_core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("POSTGRES_NAME"),
-        'USER': os.getenv("POSTGRES_USER"),
-        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-        'HOST': os.getenv("POSTGRES_HOST"),
-        'PORT': os.getenv("POSTGRES_PORT"),
+        'NAME': 'db_transcendence',
+        'USER': 'cavalinho',
+        'PASSWORD': 'root',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
