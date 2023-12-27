e4da07431270d7cdef34eccbd928cc3ccf1bb287
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2023-12-27 00:12:26 -0300

    eureka

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-coverage=$(awk '/T/ {sub(/%/, "", $(NF)); printf "%.0f", $(NF)}' teste.txt)
+coverage=$(coverage report -m --omit=*/__init__.py,*urls.py,*factories.py,*/tests/*,*/migrations/*,*/models.py,*apps.py,*admin.py,srcs_core/*,*/forms.py | awk '/T/ {sub(/%/, "", $(NF)); printf "%.0f", $(NF)}')
 
 if [[ -n "$coverage" && "$coverage" -lt 80 ]]; then
     exit 1
