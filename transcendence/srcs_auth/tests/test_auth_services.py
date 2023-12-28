afa013b3db66bebae41f22631d51a40e5e7aaa85
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2023-12-28 20:38:06 -0300

    auth_services tests for unhappy paths

--- transcendence/srcs_auth/tests/test_auth_services.py
+++ transcendence/srcs_auth/tests/test_auth_services.py
@@ -3,14 +3,55 @@ from srcs_auth import services
 import requests
 from mock import patch, Mock
 import json
+import django.http as http
 
 class TestAuthServices(TestCase):
 
     @patch.object(services, 'requests', Mock(wraps=requests))
     def test_exchange_code_on_success(self):
-        services.requests.post.return_value = Mock(spec=requests.Response, json=Mock(return_value=json.loads('{"access_token": "cavalinho"}')))
-        services.requests.get.return_value = Mock(spec=requests.Response, json=Mock(return_value=json.loads('{"user": "cavalinho"}')))
+        services.requests.post.return_value = Mock(
+            spec=requests.Response,
+            status_code=200,
+            ok=True,
+            json=Mock(return_value=json.loads('{"access_token": "cavalinho"}'))
+        )
+        services.requests.get.return_value = Mock(
+            spec=requests.Response,
+            status_code=200,
+            ok=True,
+            json=Mock(return_value=json.loads('{"user": "cavalinho"}'))
+        )
         
         result = services.exchange_code('cavalinho')
 
         self.assertEqual(result['user'], 'cavalinho')
+
+    @patch.object(services, 'requests', Mock(wraps=requests))
+    def test_exchange_code_on_fail_at_token_post(self):
+        services.requests.post.return_value = Mock(
+            spec=requests.Response,
+            status_code=401
+            ok=False
+            json=Mock(return_value=json.loads('{}'))
+        )
+        
+        self.assertRaises(Exception, services.exchange_code, 'cavalinho')
+
+    @patch.object(services, 'requests', Mock(wraps=requests))
+    def test_exchange_code_on_fail_at_user_get(self):
+        services.requests.post.return_value = Mock(
+            spec=requests.Response,
+            status_code=200,
+            ok=True,
+            json=Mock(return_value=json.loads('{"access_token": "cavalinho"}'))
+        )
+
+        services.requests.get.return_value = Mock(
+            spec=requests.Response,
+            status_code=401
+            ok=False
+            json=Mock(return_value=json.loads('{}'))
+        )
+
+        self.assertRaises(Exception, services.exchange_code, 'cavalinho')
+
