0f4e2f0455c1b6559387effd84fa6933f86e17b6
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2023-12-28 20:08:33 -0300

    test for srcs_auth

---
+++ transcendence/srcs_auth/tests/test_auth_services.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from srcs_auth import services
+import requests
+from mock import patch, Mock
+import json
+
+class TestAuthServices(TestCase):
+
+    @patch.object(services, 'requests', Mock(wraps=requests))
+    def test_exchange_code_on_success(self):
+        services.requests.post.return_value = Mock(spec=requests.Response, json={'access_token': 'cavalinho'})
+        services.requests.get.return_value = Mock(spec=requests.Response, json={'user': 'cavalinho'})
+        
+        result = services.exchange_code('cavalinho')
+
+        self.assertEqual(result['user'], 'cavalinho')
