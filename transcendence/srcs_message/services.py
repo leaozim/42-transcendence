0d098902936372a26fab4edf2e78840ecd917dd9
Larissa <larissa_silva@outlook.com>
2023-12-29 17:39:23 -0300

    added service message implementation

--- transcendence/srcs_message/services.py
+++ transcendence/srcs_message/services.py
@@ -5,16 +5,14 @@ from srcs_chat.services import get_validated_chat_and_user
 from srcs_user.models import User
 from srcs_message.models import Message
 
-
-
 def add_message(chat_id, content, user_id):
     chat, user = get_validated_chat_and_user(chat_id, user_id)
     
     if len(content) > 500 or not content:
         return 
-    if chat.blocked and chat.blocked_by:
+    if chat.blocked:
         return
-
+    
     message = Message.objects.create(
         chat_id=chat.id,
         content=content,
