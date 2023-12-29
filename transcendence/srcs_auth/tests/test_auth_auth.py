d798d1380f8b50c6cfff3f428530820a8266e604
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2023-12-29 14:02:06 -0300

    auth_auth

--- transcendence/srcs_auth/tests/test_auth_auth.py
+++ transcendence/srcs_auth/tests/test_auth_auth.py
@@ -3,15 +3,22 @@ from srcs_auth.auth import IntraAuthenticationBackend
 from srcs_user.tests.factories import UserFactory
 
 class TestAuthAuth(TestCase):
+    def setUp(self):
+        self.intra = IntraAuthenticationBackend()
+
+    def tearDown(self):
+        self.intra = None
+        
     def test_get_user_on_fail(self):
-        intra = IntraAuthenticationBackend()
-        user = intra.get_user(999999999)
+        user = self.intra.get_user(999999999)
 
         self.assertIsNone(user)
 
     def test_get_user_on_success(self):
-        UserFactory()
-        intra = IntraAuthenticationBackend()
-        user = intra.get_user(1)
+        uf = UserFactory()
+        user = self.intra.get_user(uf.id42)
 
         self.assertIsNotNone(user)
+
+    def test_authenticate_on_failure(self):
+        self.assertIsNone(self.intra.authenticate())
