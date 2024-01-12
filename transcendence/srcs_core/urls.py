f4a3e65a4e10f99beb2db35facb60faea8d3d2db
Paco Rocha <frocha@f-rocha.com>
2024-01-12 17:04:25 -0300

    organized files and views to prepare for front end development

--- transcendence/srcs_core/urls.py
+++ transcendence/srcs_core/urls.py
@@ -1,9 +1,11 @@
+# Local folder
+from . import views
+
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='login.html'), name='login'),
+    path('', views.IndexView, name='home'),
     path('admin/', admin.site.urls),
     path('', include(('srcs_user.urls', 'srcs_user'))),
 ]
--- transcendence/srcs_core/views/views.py
+++ transcendence/srcs_core/views/views.py
@@ -1,4 +1,13 @@
-from django.http import HttpResponse
+from django.template import loader
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.urls import reverse_lazy
 
-def index(_):
-    return HttpResponse("Olá")
+
+def IndexView(request: HttpRequest):
+    context = {"segment": "index"}
+    template = loader.get_template("index.html")
+
+    if request.user.is_anonymous:
+        return HttpResponse(template.render(context, request))
+
+    return HttpResponseRedirect(reverse_lazy("onboarding:welcome"))
