36cd9bd31fa2054659279c53e673b7ba6d5b7400
Larissa <larissa_silva@outlook.com>
2024-01-22 17:03:41 -0300

    fix: update test

--- transcendence/test.sh
+++ transcendence/test.sh
@@ -1,33 +1,9 @@
-# coverage run --source='.' /transcendence/covmanage.py test
-# coverage_report=$(coverage report -m --omit=*/__init__.py,*urls.py,*factories.py,*/tests/*,*/migrations/*,*/models.py,*apps.py,*admin.py,srcs_core/*,*/forms.py,*/views.py)
-# coverage=$(echo "$coverage_report" | awk '/T/ {sub(/%/, "", $(NF)); printf "%.0f", $(NF)}')
-
-# echo "Relatório de Cobertura:"
-# echo "$coverage_report"
-
-# if [[ -n "$coverage" && "$coverage" -lt 80 ]]; then
-#     echo "Cobertura menor que 80%: $coverage%"
-#     exit 1
-# else
-#     echo "Cobertura igual ou maior que 80%: $coverage%"
-#     exit 0
-# fi
-
-#!/bin/bash
-
-COVERAGE_DIR=/transcendence/coverage
-
-# Ajuste o caminho do arquivo .coverage
 coverage run --source='.' /transcendence/manage.py test
-coverage xml -o "$COVERAGE_DIR/coverage.xml"
-
 coverage_report=$(coverage report -m --omit=*/__init__.py,*urls.py,*factories.py,*/tests/*,*/migrations/*,*/models.py,*apps.py,*admin.py,srcs_core/*,*/forms.py,*/views.py)
 coverage=$(echo "$coverage_report" | awk '/T/ {sub(/%/, "", $(NF)); printf "%.0f", $(NF)}')
 
 echo "Relatório de Cobertura:"
 echo "$coverage_report"
-
-# Adicione o caminho para o diretório de cobertura
 
 if [[ -n "$coverage" && "$coverage" -lt 80 ]]; then
     echo "Cobertura menor que 80%: $coverage%"
@@ -36,4 +12,3 @@ else
     echo "Cobertura igual ou maior que 80%: $coverage%"
     exit 0
 fi
-
