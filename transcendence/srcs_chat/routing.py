17791cb9849228baf0b6de67344874af20118fcd
rodrigo-br <rodrigoab123@gmail.com>
2024-01-27 20:04:44 -0300

    commit

--- transcendence/srcs_chat/routing.py
+++ transcendence/srcs_chat/routing.py
@@ -1,8 +1,7 @@
 from django.urls import re_path
 
-from . import consumers
-from srcs_game import consumers as consugame
+from srcs_chat.consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<room_name>\w+)/$", ChatConsumer.as_asgi()),
 ]
