c2156b9802b52b3eb06e1f21d0599dd76eca363e
Paco Rocha <frocha@f-rocha.com>
2024-01-29 23:05:04 -0300

    home page ready to render info

--- transcendence/srcs_core/views/views.py
+++ transcendence/srcs_core/views/views.py
@@ -1,14 +1,23 @@
-from django.template import loader
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views.generic import View
 
 
-def IndexView(request: HttpRequest):
+class IndexView(View):
 
-    if request.user.is_authenticated:
+    def get(self, request: HttpRequest) -> HttpResponse:
+        if request.user.is_authenticated:
+            user = request.user
+            template_name = ("home/index.html")
+            context = {
+                "user": user
+            }
+            print(user)
+            return render(request, template_name, context)
+        else:
+            context = {"segment": "index"}
+            template = ("index.html")
 
-    else :
-        context = {"segment": "index"}
-        template = loader.get_template("index.html")
-
-        return HttpResponse(template.render(context, request))
+            return render(request, template, context)
