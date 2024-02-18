ee0ae74324801687fc71c1ca52338a339e7365b6
rodrigo-br <rodrigoab123@gmail.com>
2024-02-18 15:26:59 -0300

    add comments

--- transcendence/srcs_game/consumers.py
+++ transcendence/srcs_game/consumers.py
@@ -1,6 +1,11 @@
 import json
 import asyncio
 from channels.generic.websocket import AsyncWebsocketConsumer
+'''
+    Note que essa bola e raquete são diferentes pro python e pro js.
+    O Js não possui lógica de jogo, apenas para exibir na tela e capturar input
+    enquanto este de python contém lógicas importantes, como detecção de colisão
+'''
 from static.game.scripts.Ball import Ball
 from static.game.scripts.Paddle import Paddle
 from static.game.scripts.Vector2 import Vector2
@@ -8,41 +13,51 @@ from static.game.scripts.constants import *
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
-ball = Ball()
-left_paddle = Paddle(LEFT_PADDLE_START_POSITION[0], LEFT_PADDLE_START_POSITION[1])
-right_paddle = Paddle(RIGHT_PADDLE_START_POSITION[0], RIGHT_PADDLE_START_POSITION[1])
-score = [0, 0]
-# broadcast = None
-
+'''
+    Dicionário global que armazena todas as informações de todos os jogos.
+    Manter na key o room_group_name e como value um outro dicionário com as infos.
+    Exemplo:
+    game_69_broadcast: {
+        ball: Ball(),
+        left_paddle: Paddle(...),
+        ...
+    }
+''' 
 games = {}
 
 class BroadcastConsumer(AsyncWebsocketConsumer):
-    update_lock = asyncio.Lock()
 
+    '''
+        Após capturar o nome do grupo e o id, verifica se o nome do grupo existe como key na variavel
+        global games. Caso exista, não precisa criar de novo, então só atribui os valores para
+        as variáveis da classe (self.ball, self.paddle, ...). No caso de não existir,
+        então ele cria uma nova chave com uma nova bola, novos paddles e um score zerado.
+    '''
     async def connect(self):
         await self.accept()
         self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
         self.room_group_name = f"game_{self.room_id}_broadcast"
 
-        if self.room_group_name not in games:
-            games[self.room_group_name] = {
+        if self.room_group_name not in games:  # verifica se os dados do game não existe
+            games[self.room_group_name] = {  # caso não exista, cria uma nova chave para armazenar os dados
                 'ball': Ball(),
                 'left_paddle': Paddle(LEFT_PADDLE_START_POSITION[0], LEFT_PADDLE_START_POSITION[1]),
                 'right_paddle': Paddle(RIGHT_PADDLE_START_POSITION[0], RIGHT_PADDLE_START_POSITION[1]),
                 'score': [0, 0]
                 }
+        # passa todas as variáveis do game para uma variável específica dessa instância, para facilitar o acesso depois
         self.ball = games[self.room_group_name]['ball']
         self.left_paddle = games[self.room_group_name]['left_paddle']
         self.right_paddle = games[self.room_group_name]['right_paddle']
         self.score = games[self.room_group_name]['score']
 
-
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
+        # vai servir no futuro para enviar informações de começar jogo ou não
+        # vamos precisar criar a lógica de marcar connect dos players jogadores
         await self.send(text_data=json.dumps({"type": "playerId", "playerId": 'cavalinho'}))
 
-        # asyncio.create_task(self.game_loop())
-
+    # aqui vamos precisar criar a lógica de marcar disconect dos players jogadores
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
@@ -51,21 +66,34 @@ class BroadcastConsumer(AsyncWebsocketConsumer):
     async def game_update(self, event):
         await self.send(text_data=json.dumps(event['data']))
 
+    '''
+        Toda vez que recebe alguma informação do frontend, essa informação é processada e enviada de volta
+        apenas as posições dos objetos da cena e o score.
+        É basicamente o loop do jogo, porém quem controla o timedelta do loop é o phaser pelo frontend.
+        Lá, acontece a mesma coisa: Recebe infos, atualiza a tela e envia info pra cá, porém o frontend
+        não depende de receber dados para funcionar, ele possui o loop do phaser através do update,
+        então ele continua "correndo" mesmo com aqui engasgado.
+    '''
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
+
         await self.ball.move()
+
+        # pega informação de input dos paddles e processa as infos pra trazer nova posição dos paddles
         left_player_velocity = text_data_json.get("left_player_velocity", {})
         right_player_velocity = text_data_json.get("right_player_velocity", {})
         await self.right_paddle.set_paddle_velocity(right_player_velocity.get('x', 0), right_player_velocity.get('y', 0))
         await self.right_paddle.move()
         await self.left_paddle.set_paddle_velocity(left_player_velocity.get('x', 0), left_player_velocity.get('y', 0))
         await self.left_paddle.move()
+
+        # checa colisão com raquete ou ponto. atualiza o score se necessário
         self.ball.checkPaddleCollision(self.left_paddle.position.x, self.left_paddle.position.y, PLAYER_LEFT)
         self.ball.checkPaddleCollision(self.right_paddle.position.x, self.right_paddle.position.y, PLAYER_RIGHT)
+        
         if (self.ball.position.x > CANVAS_WIDTH):
             self.score[PLAYER_LEFT] += 1
             await self.ball.resetBall()
-
         if (self.ball.position.x < 0):
             self.score[PLAYER_RIGHT] += 1
             await self.ball.resetBall()
--- transcendence/static/game/Scene2.js
+++ transcendence/static/game/Scene2.js
@@ -13,17 +13,19 @@ class Scene2 extends Phaser.Scene {
     }
   
     async create() {
+
+      // descobre quem é o usuário para fazer verificações sobre ser leftPlayer, rightPlayer ou só espectador
       this.i_am = await this.getThisUser()
-      // this.senderSocket = (this.i_am==leftPlayer) ? this.sendPlayer1Socket : this.sendPlayer2Socket;
+
+      // instancia todos os objetos
       this.left_paddle = new Paddle(this, LEFT_PADDLE_START_POSITION.x, LEFT_PADDLE_START_POSITION.y, "paddle", (this.i_am==leftPlayer));
       this.player_left = new Player(this, this.left_paddle, PLAYER_LEFT, leftPlayer)
       this.right_paddle = new Paddle(this, RIGHT_PADDLE_START_POSITION.x, RIGHT_PADDLE_START_POSITION.y, "paddle", (this.i_am==rightPlayer));
       this.player_right = new Player(this, this.right_paddle, PLAYER_RIGHT, rightPlayer)
-      this.me = (this.i_am==leftPlayer) ? this.player_left : this.player_right
-      
-      this.paddle_height = this.left_paddle.height;
+
       this.ball = new Ball(this, CENTER_OF_SCREEN.x, CENTER_OF_SCREEN.y, "ball");
       
+      // prepara um eventData zerado. Conforme o backend processar as informações, este eventData será atualizado
       this.eventData = {
         "ball_x": this.ball.x,
         "ball_y": this.ball.y,
@@ -33,6 +35,7 @@ class Scene2 extends Phaser.Scene {
         "right_player_position_y": this.right_paddle.y,
         "score": [0, 0]
       };
+
       this.receiveSocket.onmessage = (event) => {
         console.log('receive data: ', event.data)
         this.eventData = JSON.parse(event.data);
@@ -41,13 +44,24 @@ class Scene2 extends Phaser.Scene {
 
     async update() {
       if (this.eventData) {
+        // Ele sempre tenta atualizar o score, mesmo quando não há novos pontos
+        // O motivo disso é que tentar atualizar só quando existe um ponto pode causar
+        // pontos que demoram para serem atualizados ou que só são atualizados
+        // após mais alguns pontos
         this.player_left.score = this.eventData.score[PLAYER_LEFT]
         this.player_left.updateScoreText()
         this.player_right.score = this.eventData.score[PLAYER_RIGHT]
         this.player_right.updateScoreText()
+
+        // Move todos os objetos da cena conforme os dados que foram recebidos do back
         this.ball.move(this.eventData.ball_x, this.eventData.ball_y)
         this.left_paddle.move(this.eventData.left_player_position_x, this.eventData.left_player_position_y)
         this.right_paddle.move(this.eventData.right_player_position_x, this.eventData.right_player_position_y)
+
+        // Envia dados de input pro backend. Ficará repetindo os mesmos resultados acima até que receba uma nova resposta
+        // Ou seja, se demorar muitos ticks do update para receber uma resposta, todas as linhas acima
+        // vão ficar se repetindo, garantindo que o jogo não quebre, mas como usa position e não velocity, 
+        // a tela vai apresentar lag
         this.receiveSocket.send(JSON.stringify({
           "type": "end_loop",
           "left_player_velocity": this.left_paddle.velocity_to_dict(),
