65d5d26a240db66abaff59fd7b27f7aabefa4488
gvitor-s <gvitor-s@student.42sp.org.br>
2024-03-14 20:26:42 -0300

    feat: config entrypoint to apply all migrations

--- transcendence/entrypoint.sh
+++ transcendence/entrypoint.sh
@@ -2,6 +2,15 @@
 #
 MANAGE_PATH="/transcendence/manage.py"
 
+INSTALLED_APPS="srcs_user srcs_auth srcs_tournament srcs_game srcs_chat srcs_message srcs_core"
+
+makemigrations() {
+python $MANAGE_PATH makemigrations $@ --settings=srcs_core.settings
+}
+
+makemigrations $INSTALLED_APPS
+
+
 python $MANAGE_PATH migrate --settings=srcs_core.settings
 
 exec python $MANAGE_PATH runserver --settings=srcs_core.settings 0.0.0.0:8000
