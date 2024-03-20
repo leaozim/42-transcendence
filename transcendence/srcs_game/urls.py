c5d5d66a3814918d2e8adb052c1a10910984aaf0
Paco Rocha <frocha@f-rocha.com>
2024-03-19 22:50:04 -0300

    adjusted player list view

--- transcendence/srcs_game/urls.py
+++ transcendence/srcs_game/urls.py
@@ -6,5 +6,5 @@ app_name = "srcs_game"
 urlpatterns = [
     path("<int:room_id>/", views.room, name="room"),
     path('create_game/<int:right_player_id>/', views.create_game_view, name='create_game'),
-    path("", views.users_list, name="index"),
+    path("", views.UserList.as_view(), name="index"),
 ]
