61e1df8cbcdd8f1f6125dc49ea57b630d46ac20c
gvitor-s <gvitor-s@student.42sp.org.br>
2024-04-03 16:39:42 -0300

    feat: daphne run in another container

--- transcendence/srcs_core/asgi.py
+++ transcendence/srcs_core/asgi.py
@@ -14,10 +14,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-if os.getenv("DEV", True):
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "srcs_core.settings")
-else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "srcs_core.settings_prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "srcs_core.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
